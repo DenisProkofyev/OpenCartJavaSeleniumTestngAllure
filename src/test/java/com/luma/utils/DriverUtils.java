43980efbafd2a51cf11441bb576f89fd8d704774
Denis Prokofyev <denisprokofyev@gmail.com>
2024-06-10 21:52:19 +0300

    Firefox: setBinary

--- src/test/java/com/luma/utils/DriverUtils.java
+++ src/test/java/com/luma/utils/DriverUtils.java
@@ -3,16 +3,12 @@ package com.luma.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chromium.ChromiumDriver;
-import org.openqa.selenium.chromium.ChromiumOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.Reporter;
 
 public class DriverUtils {
     private static final ChromeOptions chromeOptions;
     private static final FirefoxOptions firefoxOptions;
-//    private static final ChromiumOptions<ChromeOptions> chromiumOptions;
 
     static {
         chromeOptions = new ChromeOptions();
@@ -23,9 +19,7 @@ public class DriverUtils {
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
-    }
 
-    static {
         firefoxOptions = new FirefoxOptions();
 
         firefoxOptions.addArguments("--incognito");
@@ -34,18 +28,8 @@ public class DriverUtils {
         firefoxOptions.addArguments("--disable-gpu");
         firefoxOptions.addArguments("--no-sandbox");
         firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.setBinary("/Applications/Firefox.app/Contents/MacOS/firefox-bin");
     }
-
-//    static {
-//        chromiumOptions = chromeOptions;
-//
-//        chromiumOptions.addArguments("--incognito");
-//        chromiumOptions.addArguments("--headless");
-//        chromiumOptions.addArguments("--window-size=1920,1080");
-//        chromiumOptions.addArguments("--disable-gpu");
-//        chromiumOptions.addArguments("--no-sandbox");
-//        chromiumOptions.addArguments("--disable-dev-shm-usage");
-//    }
 
     private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver == null) {
@@ -60,18 +44,11 @@ public class DriverUtils {
         if (driver != null) {
             driver.quit();
         }
-        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
-//        if (driver == null) {
-//            return new FirefoxDriver(firefoxOptions);
-//        } else {
-//            driver.quit();
-//            return new FirefoxDriver(firefoxOptions);
-//        }
-        return firefoxDriver;
+
+        return new FirefoxDriver(firefoxOptions);
     }
 
     public static WebDriver createDriver(String browser, WebDriver driver) {
-//        System.out.println("Open the browser");
         switch (browser) {
             case "chrome" -> {
                 return createChromeDriver(driver);
@@ -81,14 +58,7 @@ public class DriverUtils {
             }
             default -> {
                 return null;
-        }
-//        if(browser.equals("chrome")) {
-//            driver = DriverUtils.createChromeDriver(driver);
-//
-//            return driver;
-//        } else {
-//            return null;
-////            System.exit(1);
+            }
         }
     }
 }
