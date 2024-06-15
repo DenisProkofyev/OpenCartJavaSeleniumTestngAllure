db12e181dcbb28879a28063a9e1976b365728239
Denis Prokofyev <denisprokofyev@gmail.com>
2024-06-15 17:28:25 +0300

    Add waiting due to Firefox

--- src/test/java/com/opencart/model/CartPage.java
+++ src/test/java/com/opencart/model/CartPage.java
@@ -3,24 +3,28 @@ package com.opencart.model;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CartPage extends BasePage {
 
-    @FindBy(css = "[name^='quantity']")
-    private WebElement addedItemQuantity;
-
     @FindBy(css = ".table-bordered [class$='left'] [href*='product_id']")
     private WebElement addedItemName;
+
+    @FindBy(css = "[name^='quantity']")
+    private WebElement addedItemQuantity;
 
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
-    public String getAddedGoodsQuantity() {
-        return addedItemQuantity.getAttribute("value");
+    public String getAddedItemName() {
+        getWait2().until(ExpectedConditions.visibilityOf(addedItemName));
+
+        return addedItemName.getText();
     }
 
-    public String getAddedItemName() {
-        return addedItemName.getText();
+    public String getAddedGoodsQuantity() {
+        getWait2().until(ExpectedConditions.visibilityOf(addedItemQuantity));
+        return addedItemQuantity.getAttribute("value");
     }
 }
