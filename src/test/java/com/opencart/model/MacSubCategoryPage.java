a572adccbeab120e151359c34e8aecfb7d9c6377
Denis Prokofyev <denisprokofyev@gmail.com>
2024-06-20 12:42:05 +0300

    Remove unused imports

--- src/test/java/com/opencart/model/MacSubCategoryPage.java
+++ src/test/java/com/opencart/model/MacSubCategoryPage.java
@@ -1,7 +1,5 @@
 package com.opencart.model;
 
-import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MacSubCategoryPage extends BaseCategoryPage {
